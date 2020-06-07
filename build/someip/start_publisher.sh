5a6bbf2b595df3753583f53b527baada1d8d089d
Amin Aramoon <amin.aramoon@gmail.com>
2020-06-06 17:57:47 -0700

    (working with local config)

---
+++ build/someip/start_publisher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+export VSOMEIP_CONFIGURATION=../../someip/local_server_config.json
+./someip_publisher
+
---
+++ build/someip/start_subscriber.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export VSOMEIP_CONFIGURATION=../../someip/local_client_config.json
+./someip_subscriber
+
